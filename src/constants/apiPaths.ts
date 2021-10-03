c76fc5ac8c63cafeed4832e80c3306fc947a6fd8
Gergii Mazur <gmazur92@gmail.com>
2021-10-03 19:10:25 +0300

    add path and minor changes'

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 
 const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://znvjwevfv0.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://31zjtpybn2.execute-api.eu-west-1.amazonaws.com/dev',
+  order: 'https://31zjtpybn2.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://8qnc0tstq3.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://31zjtpybn2.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'https://31zjtpybn2.execute-api.eu-west-1.amazonaws.com/dev',
 };
 export default API_PATHS;
